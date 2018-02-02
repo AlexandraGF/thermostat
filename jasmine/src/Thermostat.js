cc155c5b7edcc5b4974504e49087cf7f0963c2ef
Alexy <alexandra.fina@yahoo.com>
2018-02-02 12:46:24 +0000

    Clean the code from unnecessary uncomment lines

--- jasmine/src/Thermostat.js
+++ jasmine/src/Thermostat.js
@@ -7,12 +7,6 @@ var Thermostat = function (){
   this.powerSave = true;
 };
 
-
-// Thermostat.prototype.start = function () {
-//   // grades = 20;
-//   return DEFAULT_TEMP;
-// };
-
 Thermostat.prototype.reset = function () {
   return this.currentTemp = DEFAULT_TEMP;
 
@@ -35,14 +29,9 @@ Thermostat.prototype.down = function (num) {
     return this.currentTemp -= num;
 };
 
-// Thermostat.prototype.currentTemp = function () {
-//   return currentTemp;
-// };
 
 Thermostat.prototype.isPowerSave = function () {
   this.powerSave = true;
-
-
 };
 
 Thermostat.prototype.switchOff = function () {
@@ -69,81 +58,3 @@ Thermostat.prototype.usage = function () {
     return 'Low usage';
   };
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var Fizzbuzz = function(){};
-// var Length = function(){};
-//
-// Fizzbuzz.prototype._isDivisibleBy = function(num, div) {
-//   return (num % div === 0);
-// };
-//
-// Fizzbuzz.prototype.isDivisibleByThree = function (num) {
-//   return this._isDivisibleBy(num, 3);
-// };
-//
-// Fizzbuzz.prototype.isDivisibleByFive = function (num) {
-//   return this._isDivisibleBy(num, 5);
-// };
-//
-// Fizzbuzz.prototype.isDivisibleByFifteen = function (num) {
-//   return this._isDivisibleBy(num, 15);
-// };
-//
-// Fizzbuzz.prototype.display = function (num) {
-//   if (this.isDivisibleByFifteen(num)) {
-//     return "FizzBuzz";
-//   }
-//   if (this.isDivisibleByThree(num)) {
-//     return "Fizz";
-//   }
-//   if (this.isDivisibleByFive(num)) {
-//     return "Buzz";
-//   }
-//   return num
-// };
-//
-// var fizzBuzz = new Fizzbuzz();
-//
-// for (var i = 1; i <= 100; i++) {
-//   console.log(fizzBuzz.display(i));
-// }
-//
-//
-// Length.prototype.see = function (name) {
-//   if (name.length == 5 ) {
-//     return name + "hey";
-//   }
-//   return name;
-// };
-
-
-
-
-// var Fizzbuzz = function(){};
-//
-// Fizzbuzz.prototype.isDivisibleByThree = function (num) {
-//   return (num % 3 === 0);
-// };
-//
-// Fizzbuzz.prototype.isDivisibleByFive = function (num) {
-//   return (num % 5 === 0);
-// };
-//
-// Fizzbuzz.prototype.isDivisibleByFifteen = function (num) {
-//   return (num % 15 === 0);
-// };
